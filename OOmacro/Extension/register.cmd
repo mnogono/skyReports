16478a44d19031b0428e15ed0d381503d969411b
mnogono <mnogono@gmail.com>
2015-11-03 15:47:04 +0500

    scripts and extension for visual basic

---
+++ OOmacro/Extension/register.cmd
@@ -0,0 +1,5 @@
+"{{app}}\OpenOffice 4\program\unopkg" remove SiamsReportBuilder.oxt
+"{{app}}\OpenOffice 4\program\unopkg" remove SiamsReportLib.oxt
+
+"{{app}}\OpenOffice 4\program\unopkg" add -f "{{app}}\OpenOffice 4\extension\SiamsReportBuilder.oxt"
+"{{app}}\OpenOffice 4\program\unopkg" add -f "{{app}}\OpenOffice 4\extension\SiamsReportLib.oxt"
